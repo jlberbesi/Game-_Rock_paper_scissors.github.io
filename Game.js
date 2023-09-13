f7998e324563c478e8560c975a1e05b4f1aa3fc9
José Berbesi <88105477+jlberbesi@users.noreply.github.com>
2023-09-13 09:20:58 -0500

    Update Game.js

--- Game.js
+++ Game.js
@@ -1,5 +1,5 @@
 let jugador = 0;
-let pc = 2;
+let pc = 3;
 
 jugador = prompt("Elige 1 para piedra, 2 para papel, 3 para tijera");
 
@@ -13,4 +13,22 @@ if (jugador == 1) {
     alert("Elegiste tijera")
 } else {
     alert("Jugada inválida")
+}
+if (pc == 1){
+   alert("PC Elige piedra") 
+} else if ( pc == 2){
+    alert("PC Elige papel")
+} else if (pc == 3){
+    alert("PC Elige tijera")
+}
+
+// Combate
+if (pc == jugador) {
+    alert("Empate")
+} else if (jugador == 1 && pc == 3){
+    alert("Ganaste")
+} else if (jugador == 2 && pc == 1){
+    alert("Ganaste")
+} else {
+    alert("PERDISTE")
 }
